ca0fdc7c4ecf72b8f84e824543be19cf690bf54d
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-04-28 14:28:27 +0100

    got rid of entry_station and subject

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,78 +2,80 @@ require 'oystercard'
 
 describe Oystercard do
 
+  subject(:oystercard) { described_class.new }
+
   let(:station) { double :station }
 
   it 'creates an instance of Oystercard' do
-    expect(subject).to be_instance_of(Oystercard)
+    expect(oystercard).to be_instance_of(Oystercard)
   end
 
   it "tells user if they are on a journey" do
-    subject.add_money(Oystercard::MIN_BALANCE)
-    subject.touch_in(station)
-    expect(subject.on_journey?).to eq true
+    oystercard.add_money(Oystercard::MIN_BALANCE)
+    oystercard.touch_in(station)
+    expect(oystercard.on_journey?).to eq true
   end
 
   it 'remembers entry station' do
-    subject.add_money(1)
-    subject.touch_in(station)
-    expect(subject.entry_station).not_to eq nil
+    oystercard.add_money(1)
+    oystercard.touch_in(station)
+    expect(oystercard.current_journey[:entry]).not_to eq nil
   end
 
   describe '# journey_history' do
     it 'records and returns journey history' do
-      subject.add_money(Oystercard::MAX_BALANCE)
-      3.times { subject.touch_in(station); subject.touch_out(station) }
-      expect(subject.journey_history).not_to be_empty
+      oystercard.add_money(Oystercard::MAX_BALANCE)
+      3.times { oystercard.touch_in(station); oystercard.touch_out(station) }
+      expect(oystercard.journey_history).not_to be_empty
     end
   end
 
   describe '# current_journey' do
     it 'records entry and exit station' do
-      subject.add_money(Oystercard::MAX_BALANCE)
-      subject.touch_in(station)
-      subject.touch_out(station)
-      expect(subject.current_journey).to eq({:entry=>station, :exit=>station})
+      oystercard.add_money(Oystercard::MAX_BALANCE)
+      oystercard.touch_in(station)
+      oystercard.touch_out(station)
+      expect(oystercard.current_journey).to eq({:entry=>station, :exit=>station})
     end
   end
 
   describe '# add money' do
     it 'responds to add_money with 1 argument' do
-      expect(subject).to respond_to(:add_money).with(1).argument
+      expect(oystercard).to respond_to(:add_money).with(1).argument
     end
 
     it 'does not allow user to add more than maximum funds' do
-      subject.add_money(90)
-      expect { subject.add_money(1) }.to raise_error 'funds cannot be added: maximum balance £90'
+      oystercard.add_money(90)
+      expect { oystercard.add_money(1) }.to raise_error 'funds cannot be added: maximum balance £90'
     end
   end
 
   describe '# balance' do
     it 'responds to balance' do
-      subject.balance
-      expect(subject).to respond_to :balance
+      oystercard.balance
+      expect(oystercard).to respond_to :balance
     end
 
     it 'returns balance' do
-      expect(subject.balance).to eq(0)
+      expect(oystercard.balance).to eq(0)
     end
   end
  
   describe '# touch_out' do
     it 'can only touch_out if touched in' do
-      expect { subject.touch_out(station) }.to raise_error "You're not touched in"
+      expect { oystercard.touch_out(station) }.to raise_error "You're not touched in"
     end
 
     it "removes fare from balance" do
-      subject.add_money(Oystercard::MIN_BALANCE)
-      subject.touch_in(station)
-      expect{ subject.touch_out(station) }.to change {subject.balance}.by(-Oystercard::MIN_BALANCE)
+      oystercard.add_money(Oystercard::MIN_BALANCE)
+      oystercard.touch_in(station)
+      expect{ oystercard.touch_out(station) }.to change {oystercard.balance}.by(-Oystercard::MIN_BALANCE)
     end
   end
 
   describe "# touch_in" do
     it "refuses entry unless minimum balance is #{Oystercard::MIN_BALANCE}" do
-      expect { subject.touch_in(station) }.to raise_error "Insufficient funds"
+      expect { oystercard.touch_in(station) }.to raise_error "Insufficient funds"
     end
   end
 
